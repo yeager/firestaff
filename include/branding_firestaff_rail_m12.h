201a258970413150ba2a68bb6c0b8006b0bd5ef0
Daniel Nylander <daniel@danielnylander.se>
2026-05-28 22:18:44 +0200

    m12: add generated Firestaff rail artwork

---
+++ include/branding_firestaff_rail_m12.h
@@ -0,0 +1,15 @@
+#ifndef FIRESTAFF_BRANDING_FIRESTAFF_RAIL_M12_H
+#define FIRESTAFF_BRANDING_FIRESTAFF_RAIL_M12_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum { M12_FIRESTAFF_RAIL_WIDTH = 320, M12_FIRESTAFF_RAIL_HEIGHT = 778 };
+extern const unsigned char g_m12FirestaffRailRgb[M12_FIRESTAFF_RAIL_WIDTH * M12_FIRESTAFF_RAIL_HEIGHT * 3];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FIRESTAFF_BRANDING_FIRESTAFF_RAIL_M12_H */
