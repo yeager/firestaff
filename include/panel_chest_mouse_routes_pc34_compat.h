621150feebaafdbe6a1bdb1d759761685ae45832
Firestaff Watchdog <firestaff-watchdog@yeager>
2026-06-08 18:03:21 +0200

    Harden DM1 chest panel route geometry
    (cherry picked from commit f8faedb4dd48ab6a00df5619e891b29aca8a4040)

--- include/panel_chest_mouse_routes_pc34_compat.h
+++ include/panel_chest_mouse_routes_pc34_compat.h
@@ -1,5 +1,27 @@
 #ifndef PANEL_CHEST_MOUSE_ROUTES_PC34_COMPAT_H
 #define PANEL_CHEST_MOUSE_ROUTES_PC34_COMPAT_H
+
+typedef struct PanelChestSlotRoutePc34Compat {
+    unsigned int ordinal;
+    unsigned int commandId;
+    unsigned int zoneId;
+    unsigned int slotBoxIndex;
+    unsigned int chestSlotIndex;
+    int viewportLeft;
+    int viewportRight;
+    int viewportTop;
+    int viewportBottom;
+    int panelLeft;
+    int panelRight;
+    int panelTop;
+    int panelBottom;
+    int width;
+    int height;
+} PanelChestSlotRoutePc34Compat;
+
 const char* panel_chest_mouse_routes_GetEvidence(void);
+unsigned int panel_chest_mouse_routes_GetSlotCount(void);
+int panel_chest_mouse_routes_GetSlot(unsigned int ordinal,
+                                     PanelChestSlotRoutePc34Compat* outSlot);
 unsigned int panel_chest_mouse_routes_GetInvariant(void);
 #endif
