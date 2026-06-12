526e70a315dc61f62a125f2c26da1a7ba5f40a24
Firestaff Worker <worker@firestaff.local>
2026-06-13 01:16:50 +0200

    pass787 [csb-v1-viewport-d1c-f0108-floor-ceiling-ornament] add D1C F0108 source-lock gate

--- tests/test_csb_v1_viewport_d1c_f0108_floor_ceiling_ornament_pc34_compat.c
+++ tests/test_csb_v1_viewport_d1c_f0108_floor_ceiling_ornament_pc34_compat.c
@@ -1,4 +1,4 @@
-#include "csb_v1_viewport_d1c_f0108_floor_ceiling_ornament_pc34_compat.h"
+#include "firestaff/csb/v1/viewport/d1c_f0108_floor_ceiling_ornament_pc34_compat.h"
 
 #include <stdio.h>
 
