83db35b76f87c179fefd5a9158f23d65144f84e2
Firestaff Watchdog <watchdog@firestaff>
2026-06-12 20:32:53 +0200

    pass777 [dm1-v1-viewport-d3c-f0107-wall-ornament] source-lock

---
+++ include/firestaff/dm1/v1/viewport/d3c_f0107_wall_ornament_pc34_compat.h
@@ -0,0 +1,198 @@
+#ifndef FIRESTAFF_DM1_V1_VIEWPORT_D3C_F0107_WALL_ORNAMENT_PC34_COMPAT_H
+#define FIRESTAFF_DM1_V1_VIEWPORT_D3C_F0107_WALL_ORNAMENT_PC34_COMPAT_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DM1_V1_D3C_F0107_FRAMEBUFFER_WIDTH_PC34 320
+#define DM1_V1_D3C_F0107_FRAMEBUFFER_HEIGHT_PC34 200
+#define DM1_V1_D3C_F0107_VIEWPORT_WIDTH_PC34 224
+#define DM1_V1_D3C_F0107_VIEWPORT_HEIGHT_PC34 136
+#define DM1_V1_D3C_F0107_C10_COLOR_FLESH_PC34 10
+#define DM1_V1_D3C_F0107_ORDINAL_COUNT_PC34 6
+#define DM1_V1_D3C_F0107_STEP_COUNT_PC34 8
+#define DM1_V1_D3C_F0107_REJECTED_CONTRACT_COUNT_PC34 7
+
+typedef enum {
+    DM1_V1_D3C_F0107_STEP_F0128_D3L_DISPATCH_PC34 = 0,
+    DM1_V1_D3C_F0107_STEP_F0128_D3R_DISPATCH_PC34,
+    DM1_V1_D3C_F0107_STEP_F0128_D3C_DISPATCH_PC34,
+    DM1_V1_D3C_F0107_STEP_F0118_D3C_BODY_PC34,
+    DM1_V1_D3C_F0107_STEP_WALL_BODY_PC34,
+    DM1_V1_D3C_F0107_STEP_F0107_FRONT_WALL_ORNAMENT_PC34,
+    DM1_V1_D3C_F0107_STEP_F0108_KEEP_OUT_PC34,
+    DM1_V1_D3C_F0107_STEP_SYNTHETIC_FRAMEBUFFER_PC34
+} DM1_V1_D3CF0107StepKindPc34;
+
+typedef struct {
+    DM1_V1_D3CF0107StepKindPc34 step;
+    int order_index;
+    int expected_present;
+    const char *name;
+    const char *redmcsb_anchor;
+} DM1_V1_D3CF0107StepPc34;
+
+typedef struct {
+    int ordinal_index_c0_to_c5;
+    int sensor_ordinal;
+    int aspect_slot;
+    int view_wall;
+    int accepted_by_f0107_body;
+    int reaches_d3c_f0107;
+    const char *name;
+    const char *redmcsb_anchor;
+} DM1_V1_D3CF0107OrdinalPc34;
+
+typedef struct {
+    const char *name;
+    int relative_depth;
+    int relative_lateral;
+    int wall_zone_first;
+    int wall_zone_second;
+    int view_wall_first;
+    int view_wall_second;
+    int x;
+    int y;
+    int width;
+    int height;
+    const char *redmcsb_anchor;
+} DM1_V1_D3CF0107RejectedContractPc34;
+
+typedef struct {
+    uint8_t before;
+    uint8_t source;
+    uint8_t after;
+    int transparent_skip;
+    int writes_pixel;
+    const char *redmcsb_anchor;
+} DM1_V1_D3CF0107PixelPc34;
+
+typedef struct {
+    int framebuffer_width;
+    int framebuffer_height;
+    int viewport_width;
+    int viewport_height;
+    int viewport_x_first;
+    int viewport_y_first;
+    int viewport_x_last;
+    int viewport_y_last;
+    int view_square_d3c;
+    int relative_depth;
+    int relative_lateral;
+    int c_coordinate;
+    int y_coordinate;
+    int view_wall_d3c_front;
+    int wall_zone_d3c;
+    int wall_index_d3c;
+    int floor_view_d3c;
+    int front_wall_ornament_slot;
+    int first_thing_slot;
+    int requested_wall_zone_note_is_mismatch;
+    int f0128_d3l_draw_line;
+    int f0128_d3r_draw_line;
+    int f0128_d3c_draw_line;
+    int f0128_after_d3l_d3r;
+    int f0128_before_d2;
+    int body_function_start_line;
+    int body_function_end_line;
+    int wall_case_line;
+    int wall_draw_first_line;
+    int wall_draw_last_line;
+    int f0107_call_line;
+    int f0107_alcove_order_line;
+    int wall_case_return_line;
+    int f0108_contrast_line;
+    int f0107_zero_ordinal_returns_false;
+    int f0107_non_alcove_returns_false;
+    int f0107_alcove_returns_true;
+    int f0107_blit_uses_c10;
+    int c10_preserves_destination;
+    int c0_to_c5_ordinals_pinned;
+    int only_m552_reaches_d3c;
+    int wall_ornament_zone_base;
+    int wall_ornament_zone_stride;
+    int wall_ornament_coordinate_set;
+    int wall_ornament_zone_d3c_front;
+    int source_locked_contract_only;
+    int no_original_dos_pixel_parity;
+    int no_graphics_dat_reads;
+    DM1_V1_D3CF0107StepPc34 steps[DM1_V1_D3C_F0107_STEP_COUNT_PC34];
+    DM1_V1_D3CF0107OrdinalPc34 ordinals[DM1_V1_D3C_F0107_ORDINAL_COUNT_PC34];
+    DM1_V1_D3CF0107RejectedContractPc34 rejected[DM1_V1_D3C_F0107_REJECTED_CONTRACT_COUNT_PC34];
+    DM1_V1_D3CF0107PixelPc34 pixels[DM1_V1_D3C_F0107_ORDINAL_COUNT_PC34];
+    uint32_t deterministic_hash;
+    const char *source_evidence;
+    const char *disjointness_note;
+} DM1_V1_D3CF0107WallOrnamentModelPc34;
+
+typedef struct {
+    int writes;
+    int transparent_skips;
+    int touched_pixels;
+    int d3c_probe_overlaps_rejected_contract;
+    int rejected_contract_pixels_changed;
+    uint32_t framebuffer_hash;
+} DM1_V1_D3CF0107FramebufferProbePc34;
+
+bool dm1_v1_viewport_d3c_f0107_wall_ornament_default_model_builder_pc34(
+    DM1_V1_D3CF0107WallOrnamentModelPc34 *out_model);
+
+const DM1_V1_D3CF0107WallOrnamentModelPc34 *
+dm1_v1_viewport_d3c_f0107_wall_ornament_default_model_pc34(void);
+
+uint32_t dm1_v1_viewport_d3c_f0107_wall_ornament_hash_model_pc34(
+    const DM1_V1_D3CF0107WallOrnamentModelPc34 *model);
+
+uint32_t dm1_v1_viewport_d3c_f0107_wall_ornament_deterministic_hash_pc34(void);
+
+const DM1_V1_D3CF0107StepPc34 *
+dm1_v1_viewport_d3c_f0107_wall_ornament_step_at_pc34(size_t index);
+
+const DM1_V1_D3CF0107OrdinalPc34 *
+dm1_v1_viewport_d3c_f0107_wall_ornament_ordinal_at_pc34(size_t index);
+
+const DM1_V1_D3CF0107RejectedContractPc34 *
+dm1_v1_viewport_d3c_f0107_wall_ornament_rejected_contract_at_pc34(size_t index);
+
+bool dm1_v1_viewport_d3c_f0107_wall_ornament_accepts_sensor_ordinal_pc34(
+    int ornament_index_c0_to_c5);
+
+bool dm1_v1_viewport_d3c_f0107_wall_ornament_returns_alcove_pc34(
+    int wall_ornament_ordinal,
+    bool dungeon_classifies_alcove);
+
+int dm1_v1_viewport_d3c_f0107_wall_ornament_zone_pc34(
+    int coordinate_set,
+    int view_wall);
+
+uint8_t dm1_v1_viewport_d3c_f0107_wall_ornament_blend_pixel_pc34(
+    uint8_t destination_pixel,
+    uint8_t source_pixel,
+    uint8_t transparent_color);
+
+bool dm1_v1_viewport_d3c_f0107_wall_ornament_boxes_overlap_pc34(
+    int ax,
+    int ay,
+    int aw,
+    int ah,
+    const DM1_V1_D3CF0107RejectedContractPc34 *b);
+
+bool dm1_v1_viewport_d3c_f0107_wall_ornament_probe_framebuffer_pc34(
+    uint8_t *framebuffer,
+    size_t framebuffer_len,
+    DM1_V1_D3CF0107FramebufferProbePc34 *out_probe);
+
+const char *dm1_v1_viewport_d3c_f0107_wall_ornament_source_evidence_pc34(void);
+
+const char *dm1_v1_viewport_d3c_f0107_wall_ornament_disjointness_note_pc34(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
