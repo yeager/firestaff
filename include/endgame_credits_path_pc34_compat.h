d73d8900aaf822a0fecdfc3fd66c3c301bf7d902
Daniel Nylander <daniel@danielnylander.se>
2026-07-02 20:40:58 +0200

    pass659 dm1 wire f0444 credits path gate

--- include/endgame_credits_path_pc34_compat.h
+++ include/endgame_credits_path_pc34_compat.h
@@ -1,5 +1,20 @@
 #ifndef ENDGAME_CREDITS_PATH_PC34_COMPAT_H
 #define ENDGAME_CREDITS_PATH_PC34_COMPAT_H
+
+typedef struct EndgameCreditsPathCompat {
+    unsigned int fadesCreditsPalette;
+    unsigned int discardsInputBeforeWait;
+    unsigned int waitsForKeyboardOrMouseInput;
+    unsigned int reenterRestartRequiresRestartAllowed;
+    unsigned int reenterRestartRequiresDoNotDrawCreditsOnly;
+    unsigned int reenterRestartClearsWaitBeforeDrawingRestart;
+} EndgameCreditsPathCompat;
+
 const char* endgame_credits_path_GetEvidence(void);
+const EndgameCreditsPathCompat* endgame_credits_path_GetContract(void);
+unsigned int endgame_credits_path_ShouldReenterRestartAfterCredits(
+    unsigned int restartAllowed,
+    unsigned int doNotDrawCreditsOnly);
 unsigned int endgame_credits_path_GetInvariant(void);
+
 #endif
