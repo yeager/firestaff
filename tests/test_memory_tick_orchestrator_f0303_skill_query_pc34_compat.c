f1df4c903fc19d06fb29d7801ffc834cc9df0bf8
Daniel Nylander <daniel@danielnylander.se>
2026-07-09 13:14:27 +0200

    pass dm1 projectile champion materialization proof

--- tests/test_memory_tick_orchestrator_f0303_skill_query_pc34_compat.c
+++ tests/test_memory_tick_orchestrator_f0303_skill_query_pc34_compat.c
@@ -1958,6 +1958,100 @@ static void test_orch_projectile_wall_impact_materializes_associated_weapon(void
     assert(world.timeline.count == 0);
 }
 
+static void test_orch_projectile_champion_hit_materializes_on_impact_square(void) {
+    struct GameWorld_Compat world;
+    struct DungeonThings_Compat things;
+    struct DungeonWeapon_Compat weapons[2];
+    struct DungeonJunk_Compat junks[2];
+    struct DungeonDatState_Compat dungeon;
+    struct DungeonMapDesc_Compat maps[1];
+    struct DungeonMapTiles_Compat tiles[1];
+    unsigned char squareData[12];
+    unsigned short squareFirstThings[2];
+    struct ProjectileCreateInput_Compat createIn;
+    struct TimelineEvent_Compat firstMove;
+    struct TickInput_Compat input;
+    struct TickResult_Compat result;
+    unsigned short materializedThing;
+    int slot = -1;
+    int i;
+
+    init_world(&world, &things, weapons, junks);
+    memset(&dungeon, 0, sizeof(dungeon));
+    memset(maps, 0, sizeof(maps));
+    memset(tiles, 0, sizeof(tiles));
+    memset(squareFirstThings, 0, sizeof(squareFirstThings));
+    for (i = 0; i < 12; ++i) {
+        squareData[i] = square_for_test(DUNGEON_ELEMENT_CORRIDOR, 0);
+    }
+    squareData[(1 * 3) + 1] =
+        square_for_test(DUNGEON_ELEMENT_CORRIDOR,
+                        DUNGEON_SQUARE_MASK_THING_LIST);
+    squareData[(2 * 3) + 1] =
+        square_for_test(DUNGEON_ELEMENT_CORRIDOR,
+                        DUNGEON_SQUARE_MASK_THING_LIST);
+    squareFirstThings[0] = THING_ENDOFLIST;
+    squareFirstThings[1] = THING_ENDOFLIST;
+    weapons[0].type = 27; /* ReDMCSB C27_WEAPON_ARROW. */
+    weapons[0].next = THING_NONE;
+
+    dungeon.header.mapCount = 1;
+    dungeon.header.squareFirstThingCount = 2;
+    dungeon.maps = maps;
+    dungeon.tiles = tiles;
+    dungeon.tilesLoaded = 1;
+    maps[0].width = 4;
+    maps[0].height = 3;
+    tiles[0].squareData = squareData;
+    tiles[0].squareCount = 12;
+    world.dungeon = &dungeon;
+    world.newPartyMapIndex = -1;
+    world.gameTick = 101;
+    world.timeline.nowTick = 101;
+    world.party.mapIndex = 0;
+    world.partyMapIndex = 0;
+    world.party.mapX = 2;
+    world.party.mapY = 1;
+    world.party.champions[0].hp.current = 100;
+    world.party.champions[0].hp.maximum = 100;
+    things.squareFirstThings = squareFirstThings;
+    things.squareFirstThingCount = 2;
+
+    memset(&createIn, 0, sizeof(createIn));
+    createIn.category = PROJECTILE_CATEGORY_KINETIC;
+    createIn.subtype = PROJECTILE_SUBTYPE_KINETIC_ARROW;
+    createIn.ownerKind = PROJECTILE_OWNER_CHAMPION;
+    createIn.ownerIndex = 0;
+    createIn.mapIndex = 0;
+    createIn.mapX = 1;
+    createIn.mapY = 1;
+    createIn.cell = 1;
+    createIn.direction = 1;
+    createIn.kineticEnergy = 80;
+    createIn.attack = 40;
+    createIn.stepEnergy = 10;
+    createIn.currentTick = 100;
+    createIn.firstMoveGraceFlag = 1;
+    createIn.associatedThing = make_thing(THING_TYPE_WEAPON, 0);
+    assert(F0810_PROJECTILE_Create_Compat(
+        &createIn, &world.projectiles, &slot, &firstMove) == 1);
+    assert(F0721_TIMELINE_Schedule_Compat(&world.timeline, &firstMove) == 1);
+
+    memset(&input, 0, sizeof(input));
+    memset(&result, 0, sizeof(result));
+    assert(F0884_ORCH_AdvanceOneTick_Compat(&world, &input, &result) == ORCH_OK);
+
+    /* ReDMCSB PROJEXPL.C F0219 lines 687-743 commits champion-impact
+     * coordinates before F0217 reaches F0215 materialization. */
+    materializedThing =
+        (unsigned short)(make_thing(THING_TYPE_WEAPON, 0) | (0u << 14));
+    assert(world.projectiles.count == 0);
+    assert(squareFirstThings[0] == THING_ENDOFLIST);
+    assert(squareFirstThings[1] == materializedThing);
+    assert(weapons[0].next == THING_ENDOFLIST);
+    assert(world.party.champions[0].hp.current < 100);
+}
+
 static void test_orch_projectile_wall_impact_appends_associated_weapon_raw_tail(void) {
     struct GameWorld_Compat world;
     struct DungeonThings_Compat things;
@@ -7161,6 +7255,7 @@ int main(void) {
     test_orch_magical_wall_impact_nonzero_adjusted_explosion_spawns();
     test_orch_projectile_wall_impact_emits_non_explosion_sound();
     test_orch_projectile_wall_impact_materializes_associated_weapon();
+    test_orch_projectile_champion_hit_materializes_on_impact_square();
     test_orch_projectile_wall_impact_appends_associated_weapon_raw_tail();
     test_orch_projectile_portcullis_uses_object_allowed_slots();
     test_orch_slime_wall_impact_emits_wooden_thud_without_explosion();
