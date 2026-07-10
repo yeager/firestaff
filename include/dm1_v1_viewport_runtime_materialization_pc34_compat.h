192e702d26654737b0536781714713cb1b0661be
Daniel Nylander <daniel@danielnylander.se>
2026-07-10 11:01:46 +0200

    dm1: unify viewport materialization decisions

---
+++ include/dm1_v1_viewport_runtime_materialization_pc34_compat.h
@@ -0,0 +1,62 @@
+#ifndef FIRESTAFF_DM1_V1_VIEWPORT_RUNTIME_MATERIALIZATION_PC34_COMPAT_H
+#define FIRESTAFF_DM1_V1_VIEWPORT_RUNTIME_MATERIALIZATION_PC34_COMPAT_H
+
+/*
+ * DM1 V1 visible viewport materialization receipt.
+ *
+ * ReDMCSB: DUNGEON.C F0172 publishes square facts; DUNVIEW.C F0115
+ * (4547-4581, 4923, 5075, 5668-5683, 5915-5933) consumes those facts in
+ * distinct wall, object, projectile and deferred-explosion passes.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum DM1_V1_ViewportRuntimeOriginPc34 {
+    DM1_V1_VIEWPORT_RUNTIME_ORIGIN_NEW_START_PC34 = 0,
+    DM1_V1_VIEWPORT_RUNTIME_ORIGIN_ORIGINAL_SAVE_PC34 = 1,
+    DM1_V1_VIEWPORT_RUNTIME_ORIGIN_QUICKSAVE_RESUME_PC34 = 2
+} DM1_V1_ViewportRuntimeOriginPc34;
+
+typedef struct DM1_V1_ViewportRuntimeMaterializationInputPc34 {
+    int relativeForward;
+    int relativeSide;
+    int elementType;
+    int floorItemCount;
+    int projectileCount;
+    int projectileCell;
+    int hasVisibleChampionMirrorPayload;
+    DM1_V1_ViewportRuntimeOriginPc34 runtimeOrigin;
+} DM1_V1_ViewportRuntimeMaterializationInputPc34;
+
+typedef struct DM1_V1_ViewportRuntimeMaterializationDecisionPc34 {
+    int valid;
+    int consumedF0172SquareFacts;
+    int consumedF0115ThingPass;
+    int viewSquare;
+    int row;
+    int itemZone;
+    int projectileZone;
+    int drawFloorItems;
+    int drawRuntimeProjectiles;
+    int drawDeferredSpellEffects;
+    int suppressMaterializedItemPayload;
+    int suppressMirrorAsFloorItem;
+    int suppressMirrorAsProjectile;
+    int suppressMirrorAsSpellEffect;
+    int noM11Fallback;
+    const char *sourceAnchor;
+} DM1_V1_ViewportRuntimeMaterializationDecisionPc34;
+
+int dm1_v1_viewport_runtime_materialization_decide_pc34(
+    const DM1_V1_ViewportRuntimeMaterializationInputPc34 *input,
+    DM1_V1_ViewportRuntimeMaterializationDecisionPc34 *outDecision);
+
+const char *dm1_v1_viewport_runtime_materialization_source_evidence_pc34(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FIRESTAFF_DM1_V1_VIEWPORT_RUNTIME_MATERIALIZATION_PC34_COMPAT_H */
