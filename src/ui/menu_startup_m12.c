291fe5db9cdfe2da0ee42f027a2534dd36e99cd1
Daniel Nylander <daniel@danielnylander.se>
2026-07-13 10:59:04 +0200

    release: prepare v3.0.74

--- src/ui/menu_startup_m12.c
+++ src/ui/menu_startup_m12.c
@@ -5,7 +5,7 @@
 #include "firestaff_retroachievements.h"
 #include "menu_startup_a11y_m12.h"
 
-#define FIRESTAFF_VERSION_STRING "v3.0.73"
+#define FIRESTAFF_VERSION_STRING "v3.0.74"
 #include "firestaff_bestiary.h"
 #include "screenshot_gallery_m12.h"
 #include "firestaff_spell_ref.h"
