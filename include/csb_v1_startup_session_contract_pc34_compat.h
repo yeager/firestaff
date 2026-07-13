713cf2397e2f726aaa784e29978af4cae386c4f3
Daniel Nylander <daniel@danielnylander.se>
2026-07-13 02:41:08 +0200

    CSB: gate first live HUD after C040 clear

--- include/csb_v1_startup_session_contract_pc34_compat.h
+++ include/csb_v1_startup_session_contract_pc34_compat.h
@@ -13,9 +13,30 @@ typedef struct CSB_V1_StartupSessionTerminalReceipt_PC34 {
     unsigned int session_generation;
 } CSB_V1_StartupSessionTerminalReceipt_PC34;
 
+typedef struct CSB_V1_StartupSessionLiveHudReceipt_PC34 {
+    int valid;
+    int c040_cleared_once;
+    int c017_live_base_only;
+    int c017_source_asset_id;
+    int c017_width;
+    int c017_height;
+    int special_palette;
+    unsigned int source_tick;
+    unsigned int session_generation;
+} CSB_V1_StartupSessionLiveHudReceipt_PC34;
+
 /* ReDMCSB TITLE.C F0437, ENTRANCE.C F0807, PANEL.C F0347. */
 int csb_v1_startup_session_terminal_receipt_pc34(
     const CSB_V1_StartupRuntimeAssetSession_PC34 *session,
     CSB_V1_StartupSessionTerminalReceipt_PC34 *out_receipt);
+
+/* ReDMCSB PANEL.C F0346/F0347: one C040 clear returns to neutral C017. */
+int csb_v1_startup_session_live_hud_receipt_pc34(
+    const CSB_V1_StartupRuntimeAssetSession_PC34 *session,
+    const CSB_V1_StartupSessionTerminalReceipt_PC34 *terminal_receipt,
+    unsigned int c040_clear_count,
+    unsigned int source_tick,
+    unsigned int session_generation,
+    CSB_V1_StartupSessionLiveHudReceipt_PC34 *out_receipt);
 
 #endif
