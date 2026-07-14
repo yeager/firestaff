b6b8dbb1b218a8ba26651d12747fbc37d179be31
Daniel Nylander <daniel@danielnylander.se>
2026-07-14 14:09:28 +0200

    feat: map ReDMCSB F0934 hex digits

---
+++ include/redmcsb_f0934_convert_value_to_hex_digits.h
@@ -0,0 +1,9 @@
+#ifndef REDMCSB_F0934_CONVERT_VALUE_TO_HEX_DIGITS_H
+#define REDMCSB_F0934_CONVERT_VALUE_TO_HEX_DIGITS_H
+
+#include <stdint.h>
+
+/* ReDMCSB PRIM1.C F0934_ConvertValueToHexDigits. */
+char *redmcsb_f0934_convert_value_to_hex_digits(char *string, uint32_t value);
+
+#endif
