b041658b1ffeb66eac9803e9600bdfb49ac1a8be
Daniel Nylander <daniel@danielnylander.se>
2026-07-14 18:58:42 +0200

    Implement ReDMCSB F1033 hatch box dispatch

---
+++ include/redmcsb_f1033_hatch_box.h
@@ -0,0 +1,30 @@
+#ifndef FIRESTAFF_REDMCSB_F1033_HATCH_BOX_H
+#define FIRESTAFF_REDMCSB_F1033_HATCH_BOX_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ReDMCSB F1033 binds F1032's bitmap and width to the screen globals. */
+typedef void (*redmcsb_f1033_hatch_box_primitive_fn)(
+    uint8_t *screen_bitmap,
+    int16_t *xyz,
+    int16_t color,
+    int16_t screen_pixel_width);
+
+void redmcsb_f1033_hatch_box(
+    redmcsb_f1033_hatch_box_primitive_fn hatch_box_primitive,
+    uint8_t *screen_bitmap,
+    int16_t *xyz,
+    int16_t color,
+    int16_t screen_pixel_width);
+
+const char *redmcsb_f1033_hatch_box_source_evidence(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FIRESTAFF_REDMCSB_F1033_HATCH_BOX_H */
