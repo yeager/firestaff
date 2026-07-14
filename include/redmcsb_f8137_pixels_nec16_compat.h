30ff6b8a70078f4a342647ea6efd44396b4a8372
Daniel Nylander <daniel@danielnylander.se>
2026-07-14 21:25:46 +0200

    Implement ReDMCSB F8137 packed pixels

---
+++ include/redmcsb_f8137_pixels_nec16_compat.h
@@ -0,0 +1,6 @@
+#ifndef FIRESTAFF_REDMCSB_F8137_PIXELS_NEC16_COMPAT_H
+#define FIRESTAFF_REDMCSB_F8137_PIXELS_NEC16_COMPAT_H
+#include <stddef.h>
+#include <stdint.h>
+void redmcsb_f8137_set_multiple_pixels_nec16_compat(uint8_t *video, size_t bytes, uint16_t pixel, uint8_t color, uint16_t count);
+#endif
