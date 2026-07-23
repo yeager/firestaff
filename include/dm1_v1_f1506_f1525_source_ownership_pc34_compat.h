48460b1331bb4d88b65a578434801b5f06ffec68
Daniel Nylander <daniel@danielnylander.se>
2026-07-23 20:21:53 +0200

    dm1: gate source route ownership

---
+++ include/dm1_v1_f1506_f1525_source_ownership_pc34_compat.h
@@ -0,0 +1,31 @@
+#ifndef FIRESTAFF_DM1_V1_F1506_F1525_SOURCE_OWNERSHIP_PC34_COMPAT_H
+#define FIRESTAFF_DM1_V1_F1506_F1525_SOURCE_OWNERSHIP_PC34_COMPAT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    DM1_V1_F1506_F1525_UNASSIGNED_OR_LOCAL_PC34 = 0,
+    DM1_V1_F1506_F1525_PLATFORM_BOUNDARY_PC34 = 1
+} DM1_V1_F1506F1525OwnershipKindPc34;
+
+typedef struct {
+    unsigned int number;
+    DM1_V1_F1506F1525OwnershipKindPc34 kind;
+    const char *symbol;
+    const char *source_anchor;
+    const char *owner_or_rationale;
+} DM1_V1_F1506F1525OwnershipPc34;
+
+const DM1_V1_F1506F1525OwnershipPc34 *
+dm1_v1_f1506_f1525_source_ownership_pc34(unsigned int number);
+int dm1_v1_f1506_f1525_admits_authentic_route_pc34(unsigned int number);
+int dm1_v1_f1506_f1525_has_synthetic_route_pc34(unsigned int number);
+const char *dm1_v1_f1506_f1525_source_ownership_evidence_pc34(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
