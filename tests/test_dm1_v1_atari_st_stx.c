83b5296b65ac8e67140f396c49bd4385868d3ba1
Daniel Nylander <daniel@danielnylander.se>
2026-08-06 20:39:51 +0200

    test: validate Atari STX graphics from extracted sizes

--- tests/test_dm1_v1_atari_st_stx.c
+++ tests/test_dm1_v1_atari_st_stx.c
@@ -28,22 +28,40 @@ int main(void)
         DM1_V1_AtariStx stx;
         DM1_V1_AtariStGraphicsDat graphics;
         size_t size = 0u;
+        size_t graphics_size = 0u;
         size_t extracted_size = 0u;
         unsigned char *image = read_file(path, &size);
-        unsigned char *graphics_bytes = (unsigned char *)malloc(271911u);
-        unsigned char *dungeon_bytes = (unsigned char *)malloc(33286u);
-        assert(image && graphics_bytes && dungeon_bytes);
+        size_t graphics_capacity = 1024u * 1024u;
+        size_t dungeon_capacity = 128u * 1024u;
+        unsigned char *graphics_bytes = (unsigned char *)malloc(graphics_capacity);
+        unsigned char *dungeon_bytes = (unsigned char *)malloc(dungeon_capacity);
+        unsigned char *pixels = (unsigned char *)malloc(320u * 200u);
+        uint16_t width = 0u;
+        uint16_t height = 0u;
+        int decoded_records = 0;
+        uint16_t i;
+        assert(image && graphics_bytes && dungeon_bytes && pixels);
         assert(dm1_v1_atari_st_stx_open(image, size, &stx));
         assert(stx.sector_count == 800u);
         assert(dm1_v1_atari_st_stx_extract_file(
-            &stx, "GRAPHICS.DAT", graphics_bytes, 271911u, &extracted_size));
-        assert(extracted_size == 271911u);
+            &stx, "GRAPHICS.DAT", graphics_bytes, graphics_capacity,
+            &extracted_size));
+        assert(extracted_size > 0u);
+        graphics_size = extracted_size;
         assert(dm1_v1_atari_st_stx_extract_file(
-            &stx, "DUNGEON.DAT", dungeon_bytes, 33286u, &extracted_size));
-        assert(extracted_size == 33286u);
+            &stx, "DUNGEON.DAT", dungeon_bytes, dungeon_capacity,
+            &extracted_size));
+        assert(extracted_size > 0u);
         assert(dm1_v1_atari_st_graphics_open(
-            graphics_bytes, 271911u, &graphics));
-        assert(graphics.records[0].compressed_size != 0u);
+            graphics_bytes, graphics_size, &graphics));
+        for (i = 0u; i < DM1_V1_ATARI_ST_GRAPHICS_COUNT; ++i) {
+            if (dm1_v1_atari_st_graphics_decode(
+                    &graphics, i, pixels, 320u * 200u, &width, &height)) {
+                ++decoded_records;
+            }
+        }
+        assert(decoded_records > 0);
+        free(pixels);
         free(dungeon_bytes);
         free(graphics_bytes);
         free(image);
